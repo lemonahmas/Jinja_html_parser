dd38e5b35d79510438f07ffde55bbc198627cc04
Aaron Li <lizhimou@microsoft.com>
2023-02-24 14:09:51 +0800

    update

--- jinja_html_parser.py
+++ jinja_html_parser.py
@@ -1,5 +1,5 @@
 import jinja2
-env = jinja2.Environment(loader=jinja2.FileSystemLoader("E:\VSCPython\Jinja_html_parser"))
+env = jinja2.Environment(loader=jinja2.FileSystemLoader("./"))
 temp = env.get_template('./test.html')
 
 # https://jinja.palletsprojects.com/en/3.1.x/templates/# Check this out XD.
@@ -9,5 +9,13 @@ a_variable = "Damn this is ez."
 
 out = temp.render({"navigation":navigation,"a_variable":a_variable})
 with open("./out.html","w") as f:
+    f.writelines(out)
+    f.close()
+
+with open("./test.html") as f:
+    template = jinja2.Template(source=f.read())
+
+template.render({"navigation":navigation,"a_variable":a_variable})
+with open("./out2.html","w") as f:
     f.writelines(out)
     f.close()
